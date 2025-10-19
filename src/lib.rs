4bf07ff155f595f095508971fefc013afa40384b
rlarson20 <richard.rj.larson@gmail.com>
2025-10-19 18:27:41 -0400

    feat: AddGradFn

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,22 @@
-pub trait GradFn {}
-pub struct TensorRef {
-    //may end up as:
-    //type TensorRef = std::rc::Rc<std::cell::RefCell<Tensor>>
-    //to allow shared mut refs to parent tensors in graph
-    //multiple child tensors can refer to parent
-    //during backprop can mutate parents through refs
-    //using Rc<RefCell<Tensor>> is simple approach for dyn graph
-    //for thread-safety, use Arc<Mutex<Tensor>>
-    //starting single-threaded
+type TensorRef = std::rc::Rc<std::cell::RefCell<Tensor>>;
+//to allow shared mut refs to parent tensors in graph
+//multiple child tensors can refer to parent
+//during backprop can mutate parents through refs
+//using Rc<RefCell<Tensor>> is simple approach for dyn graph
+//for thread-safety, use Arc<Mutex<Tensor>>
+//starting single-threaded
+
+trait GradFn {
+    fn backward(&self, out_grad: &Tensor, parents: &[TensorRef]) -> Vec<Option<Tensor>>;
+}
+
+struct AddGradFn;
+impl GradFn for AddGradFn {
+    fn backward(&self, out_grad: &Tensor, parents: &[TensorRef]) -> Vec<Option<Tensor>> {
+        vec![
+            Some(Tensor::new(out_grad.data.clone(), &out_grad.shape, false)),
+            Some(Tensor::new(out_grad.data.clone(), &out_grad.shape, false)),
+        ]
 }
 
 pub enum Device {
@@ -66,6 +75,115 @@ impl Tensor {
     //create tensor, print to verify shape and data align
     //requires_grad stays false for now
     //enable autograd in later steps
+    //
+    //next step: arithmetic ops on tensors
+    //start with element wise
+    //forward manner first
+    //
+    //for each, add:
+    //num computation, eg adding 2 tensors element wise
+    //building autograd graph:
+    //new out tensor recording how it was computed from inputs
+    //(eg, parents, grad_fn)
+    //
+    //shape compat:
+    //first require exact shape matches for bin op
+    //later add broadcasting a la numpy
+    pub fn add(&self, other: &Tensor) -> Tensor {
+        assert!(self.shape == other.shape, "Shapes must be equal to add");
+        let result_data: Vec<f32> = self
+            .data
+            .iter()
+            .zip(other.data.iter())
+            .map(|(a, b)| a + b)
+            .collect();
+        let mut out = Tensor::new(
+            result_data,
+            &self.shape,
+            self.requires_grad || other.requires_grad,
+        );
+        if out.requires_grad {
+            let self_ref = TensorRef::new(RefCell::new(self.clone()));
+            let other_ref = TensorRef::new(RefCell::new(other.clone()));
+            out.parents = vec![self_ref, other_ref];
+            out.grad_fn = Some(Box::new(AddGradFn));
+        }
+        out
+    }
+    pub fn sub(&self, other: &Tensor) -> Tensor {
+        assert!(self.shape == other.shape, "Shapes must be equal to add");
+        let result_data: Vec<f32> = self
+            .data
+            .iter()
+            .zip(other.data.iter())
+            .map(|(a, b)| a - b)
+            .collect();
+        let mut out = Tensor::new(
+            result_data,
+            &self.shape,
+            self.requires_grad || other.requires_grad,
+        );
+        if out.requires_grad {
+            let self_ref = TensorRef::new(RefCell::new(self.clone()));
+            let other_ref = TensorRef::new(RefCell::new(other.clone()));
+            out.parents = vec![self_ref, other_ref];
+            out.grad_fn = Some(Box::new(SubGradFn));
+        }
+        out
+    }
+    pub fn elem_mul(&self, other: &Tensor) -> Tensor {
+        assert!(self.shape == other.shape, "Shapes must be equal to add");
+        let result_data: Vec<f32> = self
+            .data
+            .iter()
+            .zip(other.data.iter())
+            .map(|(a, b)| a * b)
+            .collect();
+        let mut out = Tensor::new(
+            result_data,
+            &self.shape,
+            self.requires_grad || other.requires_grad,
+        );
+        if out.requires_grad {
+            let self_ref = TensorRef::new(RefCell::new(self.clone()));
+            let other_ref = TensorRef::new(RefCell::new(other.clone()));
+            out.parents = vec![self_ref, other_ref];
+            out.grad_fn = Some(Box::new(ElemMulGradFn));
+        }
+        out
+    }
+    //iterating through self/other.data to compute val
+    //make new out tensor, requires_grad if either needs grad
+    //if grad needed, store refs in parents
+    //also assign grad_fn, obj knows how to prop grad for op
+    //make OpGradFn type (struct/enum var) impl trait GradFn
+    //other ops:
+    //negation, flips sign with simple grad func
+    //relu: max(0,x) elementwise, adds non-lin
+    //grad 1 for +, 0 for - during backprop
+    //
+    //matmul/dotprod: crucial
+    //basic matmul for 2D tensor:
+    //A.shape (m,n)
+    //B.shape (n,p)
+    //res.shape(m,p)
+    //
+    //triple nested loop to compute
+    //C_{i,j} = sum_k A_{i,k} * B_{k,j}
+    //to optimize later w/ BLAS or parallel loop
+    //
+    //if needed, impl tensor indexing, slicing, reshaping
+    //eg: flattening tensor, reshape for ops
+    //each op producing new tensor should have appropriate grad_fn
+    //
+    //eg:
+    //AddGradFn: in back, take grad of out, pass to parents
+    //since delta(a+b)/delta(a) = 1, delta(a+b)/delta(b) = 1,
+    //grad flows unchanged to in
+    //MulGradFn: z = x * y, delta(z)/delta(x) = y, back mul incoming grad by other parent val
+    //MatMulGradFn: more complex: grad involves transposes:
+    //if z = X dot W, grad w.rt X = grad_out dot W.transpose,
+    //and grad w.rt W = X.transpose dot grad.out
 }
 
 pub struct Tensor {
