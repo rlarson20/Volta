951f4ba7adcb6a9b35903bbae519d9564e2121e7
rlarson20 <richard.rj.larson@gmail.com>
2025-10-19 16:29:10 -0400

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+pub trait GradFn {}
+pub struct TensorRef {}
+pub struct Device {}
+
+pub struct Tensor {
+    pub data: Vec<f32>,         // flat data vec, len = prod shape dims
+    pub shape: Vec<usize>,      //tensor dims, eg [B,C,H,W]
+    pub grad: Option<Vec<f32>>, //grad w.r.t tensor data, None if req_grad == false
+    pub requires_grad: bool,
+    pub grad_fn: Option<Box<dyn GradFn>>, //func to compute grad, if result of op
+    pub parents: Vec<TensorRef>,          //refs to parent tensor on graph
+    pub device: Device,                   //cpu/gpu
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
